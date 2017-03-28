a008dd05c430e6d477864cd1572e2ddb919a4cfa
grozavu-andrei <grozavuandrei96@gmail.com>
2017-03-28 08:41:50 +0300

    Add files via upload

---
+++ Bird.java
@@ -0,0 +1,63 @@
+
+import java.util.Vector;
+
+public class Bird {
+
+    private int id;
+
+    private String name;
+
+    private String power;
+
+    private String color;
+
+    private String type;
+
+    private int damage;
+
+    public Bird(int id, String name, String power, String color, String type, int damage) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.color = color;
+        this.type = type;
+        this.damage = damage;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPower() {
+        return this.power;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public int getDamage() {
+        return this.damage;
+    }
+
+    public Integer getNextBird(int id) {
+        return null;
+    }
+
+    public Integer shot(int shotTrajectoryId) {
+        return null;
+    }
+
+    public String toString() {
+        return "Id: " + this.id + " Name: " + this.name + " Power : " + this.power + " Color: " + this.color + " Type: " + this.type + " Damage: " + this.damage;
+    }
+
+}
---
+++ Obstacle.java
@@ -0,0 +1,55 @@
+
+import java.util.Vector;
+
+public class Obstacle {
+
+    private int id;
+
+    private String type;
+
+    private String shape;
+
+    private int positionX;
+
+    private int positionY;
+
+    private int hp;
+
+    public Obstacle(int id, String type, String shape, int positionX, int positionY, int hp) {
+        this.id = id;
+        this.type = type;
+        this.shape = shape;
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.hp = hp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public String toString() {
+        return "Id: " + this.id + " Type: " + this.type + " Shape: " + this.shape + " Position: " + this.positionX + " " + this.positionY + " HP: " + this.hp;
+    }
+
+}
